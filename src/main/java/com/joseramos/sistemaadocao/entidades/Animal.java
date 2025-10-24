29f815158f99e8c4b3031f34288d44f05a2121da
José Ramos <joseramosdossantosjunior@gmail.com>
2025-10-23 21:27:53 -0300

    Modificação na class Animal

--- src/main/java/com/joseramos/sistemaadocao/entidades/Animal.java
+++ src/main/java/com/joseramos/sistemaadocao/entidades/Animal.java
@@ -1,4 +1,84 @@
 package com.joseramos.sistemaadocao.entidades;
 
-public class Animal {
+public abstract class Animal {
+    private String nome;
+    private String raca;
+    private String idade;
+    private String sexo;
+    private String porte;
+    private String descricao;
+    private boolean disponivel;
+
+    public Animal(String nome, String raca, String idade, String sexo, String porte, String descricao) {
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.porte = porte;
+        this.descricao = descricao;
+        this.disponivel = true;
+    }
+
+    public abstract void emitirSom();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getPorte() {
+        return porte;
+    }
+
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal:\n nome=" + nome + ", raca=" + raca + ", idade=" + idade + ", sexo=" + sexo + ", porte=" + porte + ", descricao=" + descricao + ", disponivel=" + disponivel;
+    }
 }
